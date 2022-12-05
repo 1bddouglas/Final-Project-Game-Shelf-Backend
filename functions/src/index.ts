53584c0040fe461495658345c52226163302747d
Brandon <1bddouglas@gmail.com>
2022-12-05 13:04:56 -0500

    backend changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,10 @@
-// TODO: set this up with the router once our API is set up
+import * as functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import accountRouter from "./routes/accountRouter";
 
-// import * as functions from 'firebase-functions';
-// import express from 'express';
-// import cors from 'cors';
-// import shoutoutRouter from './routes/shoutoutRouter';
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use("/shoutouts", shoutoutRouter);
-// export const api = functions.https.onRequest(app);
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/accounts", accountRouter);
+export const api = functions.https.onRequest(app);
--- functions/src/routes/accountRouter.ts
+++ functions/src/routes/accountRouter.ts
@@ -2,17 +2,17 @@ import express from "express";
 import { getClient } from "../db";
 import BoardGame from "../models/BoardGame";
 
-const boardGameRouter = express.Router();
+const accountRouter = express.Router();
 
 const errorResponse = (error: any, res: any) => {
   console.error("FAIL", error);
   res.status(500).json({ message: "Internal Server Error" });
 };
 
-boardGameRouter.get("/", async (req, res) => {
+accountRouter.get("/", async (req, res) => {
   try {
     const client = await getClient();
-    const cursor = client.db().collection<BoardGame>("shoutouts").find();
+    const cursor = client.db().collection<BoardGame>("accounts").find();
     const results = await cursor.toArray();
     res.json(results);
   } catch (err) {
@@ -20,4 +20,4 @@ boardGameRouter.get("/", async (req, res) => {
   }
 });
 
-export default boardGameRouter;
+export default accountRouter;
