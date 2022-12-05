686fcfa9ff6e2c94d1ffb132bb18d6ea6e090ab4
Dominic Morro <dominicmorro88@gmail.com>
2022-12-05 15:35:45 -0500

    google stuff

--- functions/src/routes/accountRouter.ts
+++ functions/src/routes/accountRouter.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getClient } from "../db";
+import Account from "../models/Account";
 import BoardGame from "../models/BoardGame";
 
 const accountRouter = express.Router();
@@ -23,7 +24,37 @@ accountRouter.get("/", async (req, res) => {
 
 // add whishlist function
 accountRouter.put("", async (req, res) => {
-  
+  // const id: string = req.params.id;
+});
+
+// make account
+accountRouter.post("/", async (req, res) => {
+  const newAccount: Account = req.body;
+  try {
+    const client = await getClient();
+    await client.db().collection<Account>("accounts").insertOne(newAccount);
+    res.status(201).json(newAccount);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+accountRouter.get("/:uid", async (req, res) => {
+  const uid = req.params.uid;
+  try {
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<Account>("accounts")
+      .findOne({ uid });
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: "account not found" });
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
 });
 
 export default accountRouter;
